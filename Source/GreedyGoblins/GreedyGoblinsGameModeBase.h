a481416298c231f8e1e146896f1b37bd82d7d170
MarkNuar <marcogiuseppe.caruso@mail.polimi.it>
2021-04-15 01:01:01 +0200

    Initial commit

---
+++ Source/GreedyGoblins/GreedyGoblinsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GreedyGoblinsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GREEDYGOBLINS_API AGreedyGoblinsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
